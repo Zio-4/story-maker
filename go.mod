b637d8e8ae3d406d8e9e59ffac64a613000e3f8e
Philip Ziolkowski <philipz4848@gmail.com>
2024-04-19 22:57:37 -0700

    Initial commit, setting up go mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zio-4/story-maker
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world!")
+
+	// fmt.Println(add(1, 2))
+}
+
+// func add(a, b int) int {
+//    return a + b
+// }
